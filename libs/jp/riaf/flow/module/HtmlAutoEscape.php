7f09b574bc362665948f483e9e027c6561e2ad86
Keisuke SATO <riafweb@gmail.com>
2010-02-07 13:31:53 +0900

    add autoescape

---
+++ libs/jp/riaf/flow/module/HtmlAutoEscape.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * htmlencode を自動で挿入するよ
+ * 
+ * @author Keisuke Sato
+ * @license New BSD License
+ */
+class HtmlAutoEscape
+{
+    /**
+     * Templateのモジュール
+     * @param string $src
+     * @param Template $template
+     */
+    public function after_template(&$src, Template $template){
+        preg_match_all('/{(\\$[\$\w][^\t]*?)}/', $src, $matches);
+        $hist = array();
+        foreach($matches[0] as $k => $match){
+            if(in_array($match, $hist)
+                || stripos($match, '{$_t_.htmlencode') === 0
+                || stripos($match, '{$t.htmlencode') === 0
+                || stripos($match, '{$t.noop(') === 0) continue;
+            $src = str_replace($match, sprintf('{$t.htmlencode(%s)}', $matches[1][$k]), $src);
+            $hist[] = $match;
+        }
+        $src = preg_replace('/{$t.noop\((\\$[\$\w][^\t]*?)\)}/', '\\1', $src);
+    }
+}
