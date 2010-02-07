a6db215ae7d97177f13282064f5707371e97352c
Keisuke SATO <riafweb@gmail.com>
2010-02-07 13:39:39 +0900

    適当に修正

--- libs/jp/riaf/flow/module/HtmlAutoEscape.php
+++ libs/jp/riaf/flow/module/HtmlAutoEscape.php
@@ -17,12 +17,14 @@ class HtmlAutoEscape
         $hist = array();
         foreach($matches[0] as $k => $match){
             if(in_array($match, $hist)
-                || stripos($match, '{$_t_.htmlencode') === 0
-                || stripos($match, '{$t.htmlencode') === 0
-                || stripos($match, '{$t.noop(') === 0) continue;
+                || stripos($match, '{$_t_.html') === 0
+                || stripos($match, '{$_t_.text') === 0
+                || stripos($match, '{$t.html') === 0
+                || stripos($match, '{$t.text') === 0
+                || stripos($match, '{$t.noop') === 0) continue;
             $src = str_replace($match, sprintf('{$t.htmlencode(%s)}', $matches[1][$k]), $src);
             $hist[] = $match;
         }
-        $src = preg_replace('/{$t.noop\((\\$[\$\w][^\t]*?)\)}/', '\\1', $src);
+        $src = preg_replace('/{\$t\.noop\((\\$[\$\w][^\t]*?)\)}/', '{\\1}', $src);
     }
 }
