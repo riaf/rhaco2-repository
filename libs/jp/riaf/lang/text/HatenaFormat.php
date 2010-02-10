ddb632d453ba849ba85d2fd8f5879de704abc30e
Keisuke SATO <riafweb@gmail.com>
2010-02-10 14:50:09 +0900

    HatenaSyntax handler, cache

--- libs/jp/riaf/lang/text/HatenaFormat.php
+++ libs/jp/riaf/lang/text/HatenaFormat.php
@@ -5,8 +5,45 @@ pear('openpear.org/HatenaSyntax-beta');
 
 class HatenaFormat
 {
+    const CACHE_EXPIRE = 86400;
+    
     public function format($str){
-        return HatenaSyntax::render($str, Text::dict(module_const('option')));
+        if(module_const('is_cache', false)){
+            $store_key = array('__hatenaformat_format', $str);
+            if(Store::has($store_key)){
+                return Store::get($store_key);
+            }
+        }
+        $str = HatenaSyntax::render($str, Text::dict(module_const('option')) + array(
+            'headerlevel' => 3,
+            'htmlescape' => true,
+            'linktitlehandler' => 'HatenaFormat::linktitleHandler',
+            'keywordlinkhandler' => 'HatenaFormat::keywordlinkHandler'));
+        if(module_const('is_cache', false)) Store::set($store_key, $str, self::CACHE_EXPIRE);
+        return $str;
+    }
+    
+    /**
+     * keywordlinkHandler
+     */
+    static public function keywordlinkHandler($keyword){
+        return url($keyword);
+    }
+    /**
+     * linktitleHandler
+     */
+    static public function linktitleHandler($url){
+        if(module_const('is_cache', false)){
+            $store_key = array('__hatenaformat_linktitlehandler', $url);
+            if(Store::has($store_key)){
+                return Store::get($store_key);
+            }
+        }
+        if(Tag::setof($title, Http::read($url), 'title')){
+            $url = $title->value();
+            if(module_const('is_cache', false)) Store::set($store_key, $url, self::CACHE_EXPIRE);
+        }
+        return $url;
     }
     /**
      * 必要な PEAR パッケージをインストールする
