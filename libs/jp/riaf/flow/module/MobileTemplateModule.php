c3e2264918a1b2d57ddd31827baf53fd849d1e7e
Keisuke SATO <riaf@nequal.jp>
2010-10-09 02:58:44 +0900

    not_mobile

--- libs/jp/riaf/flow/module/MobileTemplateModule.php
+++ libs/jp/riaf/flow/module/MobileTemplateModule.php
@@ -10,6 +10,9 @@ class MobileTemplateModule
         if (strpos($src, 'm:mobile') !== false) {
             $this->m_mobile($src, $template);
         }
+        if (strpos($src, 'm:not_mobile') !== false) {
+            $this->m_not_mobile($src, $template);
+        }
     }
     private function m_agent(&$src, Template $template) {
         while (Tag::setof($tag, $src, 'm:agent')) {
@@ -29,6 +32,16 @@ class MobileTemplateModule
     private function m_mobile(&$src, Template $template) {
         while (Tag::setof($tag, $src, 'm:mobile')) {
             if (Mobile::is_mobile()) {
+                $src = str_replace($tag->plain(), $tag->value(), $src);
+            } else {
+                $src = str_replace($tag->plain(), '', $src);
+            }
+        }
+        $src = $template->parse_vars($src);
+    }
+    private function m_not_mobile(&$src, Template $template) {
+        while (Tag::setof($tag, $src, 'm:not_mobile')) {
+            if (!Mobile::is_mobile()) {
                 $src = str_replace($tag->plain(), $tag->value(), $src);
             } else {
                 $src = str_replace($tag->plain(), '', $src);
